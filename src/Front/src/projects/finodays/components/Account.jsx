196a48ead265e2226c0cfc4acf27d859fadf40c0
igorrrain <igorabcp@gmail.com>
2021-09-18 01:40:50 +0300

    Added api integration

--- src/Front/src/projects/finodays/components/Account.jsx
+++ src/Front/src/projects/finodays/components/Account.jsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { finodaysClient } from "../../../api/httpClient";
+import useApi from "../../../api/useApi";
+import { useEffect } from "react";
 import { Accordion, Container, Card, ListGroup, Table } from "react-bootstrap";
 
 class Operation extends React.Component {
@@ -34,56 +37,90 @@ class Day extends React.Component {
     }
 }
 
-class Account extends React.Component {
-    render() {
-        var accounts = [
-            { id: 1, label: "Account 1", balance: "9999" },
-            { id: 2, label: "Account 2", balance: "4464643" }
-        ];
+const Account = () => {
+    const { loading, data, fetch } = useApi({
+        initial: [],
+        fetchData: finodaysClient.users,
+    });
+
+    useEffect(() => {
+        fetch().then(() => console.log(data));
+    }, [fetch]);
+
+    var accounts = [
+        { id: 1, label: "Account 1", balance: "9999" },
+        { id: 2, label: "Account 2", balance: "4464643" }
+    ];
+
+    var accountsNew = data.map(user => {
+        return {
+            id: user.id,
+            label: user.firstName + ' ' + user.lastName,
+            balance: user.transactions.reduce((a, b) => a + b, 0)
+        }
+    });
 
 
-        var operations_per_day = [
-            {
-                "date": "Вчера",
-                operations: [
-                    { type: "+", target: "Затинацкий И.В.", category: "Переводы", sum: "1223", time: "11:21" },
-                    { type: "-", target: "Пятерочка", category: "Супермаркеты", sum: "1223", time: "09:43" }
-                ]
-            },
-            {
-                "date": "14.09.2021",
-                operations: [
-                    { type: "-", target: "KFC", category: "Фастфуд", sum: "1223", time: "19:43" },
-                    { type: "-", target: "Делимобиль", category: "Каршеринг", sum: "1223", time: "12:22" },
-                    { type: "+", target: "Николаев И.В.", category: "Переводы", sum: "1223", time: "07:00" }
-                ]
-            }
-        ];
+    var operations_per_day = [
+        {
+            "date": "Вчера",
+            operations: [
+                { type: "+", target: "Затинацкий И.В.", category: "Переводы", sum: "1223", time: "11:21" },
+                { type: "-", target: "Пятерочка", category: "Супермаркеты", sum: "1223", time: "09:43" }
+            ]
+        },
+        {
+            "date": "14.09.2021",
+            operations: [
+                { type: "-", target: "KFC", category: "Фастфуд", sum: "1223", time: "19:43" },
+                { type: "-", target: "Делимобиль", category: "Каршеринг", sum: "1223", time: "12:22" },
+                { type: "+", target: "Николаев И.В.", category: "Переводы", sum: "1223", time: "07:00" }
+            ]
+        }
+    ];
 
-        return (
-            <Container>
-                {accounts.map(tab => (
-                    <Accordion defaultActiveKey="0" style={{ marginBottom: 20 }}>
-                        <Card>
-                            <Accordion.Toggle as={Card.Header} eventKey="0" style={{ fontSize: "18px" }}>
-                                <div style={{ fontSize: "22px" }}>Счет: {tab.label}</div>
-                                <div>Баланс: {tab.balance}</div>
-                            </Accordion.Toggle>
-                            <Accordion.Collapse eventKey="0">
-                                <ListGroup>
-                                    {operations_per_day.map(day => (
-                                        <Day date={day.date} operations={day.operations} />
-                                    ))}
-                                    {/* <ListGroup.Item as={Operation} ></ListGroup.Item>
-                                    <ListGroup.Item as={Operation}></ListGroup.Item> */}
-                                </ListGroup>
-                            </Accordion.Collapse>
-                        </Card>
-                    </Accordion>
-                ))}
-            </Container>
-        )
-    }
+
+    var operations_per_day_new = [
+        {
+            "date": "Вчера",
+            operations: [
+                { type: "+", target: "Затинацкий И.В.", category: "Переводы", sum: "1223", time: "11:21" },
+                { type: "-", target: "Пятерочка", category: "Супермаркеты", sum: "1223", time: "09:43" }
+            ]
+        },
+        {
+            "date": "14.09.2021",
+            operations: [
+                { type: "-", target: "KFC", category: "Фастфуд", sum: "1223", time: "19:43" },
+                { type: "-", target: "Делимобиль", category: "Каршеринг", sum: "1223", time: "12:22" },
+                { type: "+", target: "Николаев И.В.", category: "Переводы", sum: "1223", time: "07:00" }
+            ]
+        }
+    ];
+
+    return (
+        <Container>
+            {accounts.map(tab => (
+                <Accordion defaultActiveKey="0" style={{ marginBottom: 20 }}>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="0" style={{ fontSize: "18px" }}>
+                            <div style={{ fontSize: "22px" }}>Счет: {tab.label}</div>
+                            <div>Баланс: {tab.balance}</div>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <ListGroup>
+                                {operations_per_day.map(day => (
+                                    <Day date={day.date} operations={day.operations} />
+                                ))}
+                                {/* <ListGroup.Item as={Operation} ></ListGroup.Item>
+                                <ListGroup.Item as={Operation}></ListGroup.Item> */}
+                            </ListGroup>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
+            ))}
+        </Container>
+    )
 }
 
 export default Account;
